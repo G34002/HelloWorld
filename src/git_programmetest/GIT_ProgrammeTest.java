75b7350374e62ef0a9bda746a00edd6e45c1b511
g34002 <g34002@L401P02.pdg.esi.heb.site>
2013-09-24 09:16:13 +0200

    Branche Baker

--- src/git_programmetest/GIT_ProgrammeTest.java
+++ src/git_programmetest/GIT_ProgrammeTest.java
@@ -14,6 +14,6 @@ public class GIT_ProgrammeTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World + Branche");
     }
 }
